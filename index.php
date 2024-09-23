af4de859ce484eeb1508019f635448545efa547c
Julian <njulian982@gmail.com>
2024-09-23 13:59:49 -0300

    AddPage

---
+++ index.php
@@ -0,0 +1,349 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Julian Nuñez Portfolio</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" integrity="sha384-vSIIfh2YWi9wW0r9iZe7RJPrKwp6bG+s9QZMoITbCckVJqGCCRhc+ccxNcdpHuYu" crossorigin="anonymous">
+
+  <!-- Google Fonts -->
+    <!-- <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet"> -->
+
+  <!-- Vendor CSS Files -->
+ 
+  <link href="assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <!-- Template Main CSS File -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- <script src="//code.jivosite.com/widget/4bVdgxWpjh" async></script> -->
+    <script>
+          $(window).load(function("cargador") {
+          $(".loader").fadeOut("slow");
+          })
+    </script>
+  <style>
+  .loader {
+    position: fixed;
+    left: 0px;
+    top: 0px;
+    width: 100%;
+    height: 100%;
+    z-index: 9999;
+    background: url('assets/img/portfolio/loading.gif') 50% 50% no-repeat rgb(249,249,249);
+    opacity: .8;
+}
+
+.modal_mask {
+		position:fixed;
+		top:0;
+		right:0;
+		bottom:0;
+		left:0;
+		background-color:rgba(0,0,0,.3);
+		visibility: hidden;
+		z-index:99;
+	}
+	.content-model {
+		width:400px;
+		margin:50px auto;
+		background-color:#fff;
+		border:solid 4px #000;
+		border-radius:15px;
+		text-align:center;		
+	}
+	.modal_mask:target {
+		visibility: visible;
+	}
+  </style>
+
+</head>
+
+<body>
+<div class="loader"></div>
+  <!-- ======= Mobile nav toggle button ======= -->
+  <button type="button" class="mobile-nav-toggle d-xl-none"><i class="icofont-navigation-menu"></i></button>
+
+  <!-- ======= Header ======= -->
+  <header id="header">
+    <div class="d-flex flex-column">
+
+      <div class="profile">
+        <img src="assets/img/profile-img.jpeg" alt="" class="img-fluid rounded-circle">
+        <h1 class="text-light text-center><a href="index.html">Julian Nuñez</a></h1>
+        <div class="social-links mt-3 text-center">
+         <a href="https://www.linkedin.com/in/juliannu%C3%B1ez18/" class="linkedin" target="_blank"><i class="bx bxl-linkedin"></i></a>
+        </div>
+      </div>
+
+      <nav class="nav-menu">
+        <ul>
+          <li class="active"><a href="#hero"><i class="bx bx-home"></i> <span>Home</span></a></li>
+          
+          <li><a href="#about"><i class="bx bx-user"></i> <span>Conóceme</span></a></li>
+
+
+        </ul>
+      </nav><!-- .nav-menu -->
+      <button type="button" class="mobile-nav-toggle d-xl-none"><i class="icofont-navigation-menu"></i></button>
+
+    </div>
+  </header><!-- End Header -->
+ 
+
+
+
+
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex flex-column justify-content-center align-items-center">
+    <div class="hero-container" data-aos="fade-in">
+      <h1>Julián Nuñez</h1>
+      <!-- <p><span class="typed" data-typed-items="Developer, Freelance"></span></p> -->
+      <p><span class="typed" data-typed-items="Fullstack Developer, Freelancer"></span></p>
+
+
+    </div>
+  </section><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <section id="about" class="about">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Conóceme</h2>
+          <p>Desarrollador web fullstack con una sólida pasión por el front-end y una amplia experiencia en el back-end. 
+            Comprometido con la capacitación continua y el desarrollo en el dinámico mundo de la tecnología. 
+          </p>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-4" data-aos="fade-right">
+            <img src="assets/img/developer.gif" class="img-fluid" alt="">
+          </div>
+          <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+            <h3> Desarrollador FullStack.</h3>
+            <p class="font-italic"> No dudes en contactarme.</p>
+            <div class="row">
+              <div class="col-lg-6">
+                <ul>
+                  <!-- <li><i class="icofont-rounded-right"></i> <strong>Website:</strong> www.jnweb.online</li> -->
+                  <li><i class="icofont-rounded-right"></i> <strong>Cel:</strong> 261 - 5757812</li>
+                  <li><i class="icofont-rounded-right"></i> <strong>Localización:</strong> Mendoza, Argentina</li>
+                </ul>
+              </div>
+              <div class="col-lg-6">
+                <ul>
+                  <li><i class="icofont-rounded-right"></i> <strong>Email:</strong> njulian982@gmail.com</li>
+                  <li><i class="icofont-rounded-right"></i> <strong>Freelance:</strong> Habilitado</li>
+                  <!-- <a href='https://cafecito.app/njulian' rel='noopener' target='_blank'><img srcset='https://cdn.cafecito.app/imgs/buttons/button_3.png 1x, https://cdn.cafecito.app/imgs/buttons/button_3_2x.png 2x, https://cdn.cafecito.app/imgs/buttons/button_3_3.75x.png 3.75x' src='https://cdn.cafecito.app/imgs/buttons/button_3.png' alt='Invitame un café en cafecito.app' /></a> -->
+                </ul>
+              </div>
+            </div>
+    
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+
+    <!-- ======= Skills Section ======= -->
+    <section id="skills" class="skills section-bg">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Habilidades</h2>
+          <p>
+            
+          </p>
+        </div>
+
+        <div class="row skills-content container">
+
+          <div class="col-lg-6" data-aos="fade-up">
+
+            <div class="progress">
+              <span class="skill">HTML | CSS<i class="val">100%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">MOBILE <i class="val">60%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">JavaScript <i class="val">80%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+            <div class="progress">
+              <span class="skill">PHP<i class="val">85%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+            <div class="progress">
+              <span class="skill">LARAVEL <i class="val">85%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
+
+            
+
+            <div class="progress">
+              <span class="skill">WordPress <i class="val">80%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">React <i class="val">65%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+            <div class="progress">
+              <span class="skill">Git / SASS <i class="val">80%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+            
+            <div class="progress">
+              <span class="skill">MYSQL POSTGRESS<i class="val">80</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+            
+            <div class="progress">
+              <span class="skill">DOCKER | AWS <i class="val">60%</i></span>
+              <div class="progress-bar-wrap">
+                <div class="progress-bar" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Skills Section -->
+
+
+    <!-- Modal -->
+
+    <div id="modal2" class="modal_mask">
+        <div class="content-model">
+          <a href="#/" class="cerrar">X</a>
+          <!-- <img src="assets/img/portfolio/portfolio-9.png" alt=""> -->
+          <h3>Desarrollado con:</h3>
+          <p>Html | css | javascript | php |Woocomerce</p>
+          <p>El sitio se desarrollo de cero hasta su finalización</p>
+          <p>Stock 10 productos</p>
+          <p>Editor Gutember</p>
+          <p>wooComercer configurado con pagos</p>
+
+        </div>
+      </div>
+    <div id="modal" class="modal_mask">
+        <div class="content-model">
+          <a href="#/" class="cerrar">X</a>
+          <!-- <img src="assets/img/portfolio/portfolio-9.png" alt=""> -->
+          <h3>Desarrollado con:</h3>
+          <p>Html | css | javascript | php</p>
+          <p>El sitio se desarrollo de cero hasta su finalización</p>
+        </div>
+      </div>
+
+      <div id="modal1" class="modal_mask">
+        <div class="content-model">
+          <a href="#/" class="cerrar">X</a>
+          <!-- <img src="assets/img/portfolio/portfolio-9.png" alt=""> -->
+          <h3>Desarrollado con:</h3>
+          <p>Sass | wordpress | theme propio | php | JavaScript</p>
+          <p>* El sitio se desarrollo de cero con theme propio</p>
+          <p>* Con sus funciones correspondiente en PHP</p>
+          <p>* Trabajamos con sass</p>
+          <p>* Dinamismo con javascript</p>
+        </div>
+      </div>
+    <!-- End Modal -->
+
+
+    
+
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <div class="copyright">
+        &copy;  <strong><span>Mza - Argentina</span></strong>
+      </div>
+      <div class="credits">
+      </div>
+    </div>
+  </footer><!-- End  Footer -->
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  
+    <script type="text/javascript">
+      $(window).load(function() {
+          $(".loader").fadeOut("slow");
+      });
+</script>
+ 
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/jquery.easing/jquery.easing.min.js"></script>
+    <!-- <script src="assets/vendor/php-email-form/validate.js"></script> -->
+  <script src="assets/vendor/waypoints/jquery.waypoints.min.js"></script>
+  <script src="assets/vendor/counterup/counterup.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/venobox/venobox.min.js"></script>
+  <script src="assets/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets/vendor/typed.js/typed.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/timeline.js"></script>
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
